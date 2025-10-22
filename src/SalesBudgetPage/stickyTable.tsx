a3d419a2730f89af1825acfe7d43f89d90307bce
Katherine <katherinemironova2704>
2025-10-22 22:06:58 +0300

    added StickyTable

--- src/SalesBudgetPage/stickyTable.tsx
+++ src/SalesBudgetPage/stickyTable.tsx
@@ -1,3 +1,5 @@
+import { type HTMLAttributes } from 'react'
+
 import { cn } from '@/lib/utils'
 
 // Короче, deepSeek сказал переопределять компонент нельзя иначе обновление все сломает надо создавать свой
@@ -5,9 +7,14 @@ export const StickyTable = ({
   ref,
   className,
   ...props
-}: React.HTMLAttributes<HTMLTableElement> & { ref?: React.RefObject<HTMLTableElement | null> }) => (
+}: HTMLAttributes<HTMLTableElement> & { ref?: React.RefObject<HTMLTableElement | null> }) => (
   <div className="relative w-full">
-    {/* ← overflow-auto УБРАН */}
+    {/*ПОЧЕМУ ЭТО НОРМАЛЬНО:*/}
+    {/*StickyTable - это обертка, которая не должна содержать бизнес-логику*/}
+    {/*Заголовки будут добавляться в DataTableHeader компоненте*/}
+    {/*Это компонент UI, а не законченная таблица с данными*/}
+    {/* eslint-disable-next-line sonarjs/table-header */}
     <table ref={ref} className={cn('w-full caption-bottom text-sm', className)} {...props} />
   </div>
 )
+StickyTable.displayName = 'StickyTable'
